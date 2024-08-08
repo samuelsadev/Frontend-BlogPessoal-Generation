1dc18e857d928c2b4e4e2337dcb24f0050ea3088
samuelsadev <samuelsa.dev@gmail.com>
2024-08-08 15:31:29 -0300

    subindo atualizacoes de login e rotas conectadas com backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
+import React from 'react';
 import './App.css';
-import Home from "../src/pages/home/Home"
-import NavBar from './components/navbar/Navbar';
+
+import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Login from './pages/home/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <NavBar />
-        <div className="min-h-[60vh]">
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/home' element={<Home />}></Route>
-          </Routes>
-        </div>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
           <Footer />
-    </BrowserRouter>
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
