8e5dc9443bc02102dd7a53ef841e70d92d3d4720
samuelsadev <samuelsa.dev@gmail.com>
2024-08-16 18:49:48 -0300

    ..

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
